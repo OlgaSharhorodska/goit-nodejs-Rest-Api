ceef3f33f7a902e7e1e567ff94bc3512bbdeb902
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-12-29 18:09:22 +0200

    fix

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -3,6 +3,9 @@ import { HttpError } from '../helpers/index.js';
 const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
   if (!length) {
+    if (req.route.path === '/:contactId/favorite') {
+      return next(HttpError(400, 'missing field favorite'));
+    }
     return next(HttpError(400, 'missing fields'));
   }
   next();
--- models/Contact.js
+++ models/Contact.js
@@ -3,21 +3,22 @@ import { handleSaveError, addUpdateSettings } from "./hooks.js";
 import Joi from 'joi';
 
 const contactSchema = new Schema({
-    name: {
-      type: String,
-      required: [true, 'Set name for contact'],
-    },
-    email: {
-      type: String, 
-    },
-    phone: {
-      type: String,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-})
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  }
+},
+    {versionKey: false, timestamps: true});
   
 contactSchema.post("save", handleSaveError);
 contactSchema.pre("findOneAndUpdate", addUpdateSettings )
