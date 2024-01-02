ed0a1c8f025dd5468802b4f937ff2a2ee2e7695f
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-02 19:24:24 +0200

    add owner to schemaContact

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -2,8 +2,13 @@ import User from "../models/User.js";
 import { HttpError } from '../helpers/index.js';
 import { ctrlWrapper } from '../helpers/ctrlWrapper.js';
 
-const signup = (req, res) => {
-    
+const signup = async(req, res) => {
+    const newUser = await User.create(req.body);
+
+   res.json({
+     username: newUser.username,
+     email: newUser.email,
+   });
 }
 
 
--- models/Contact.js
+++ models/Contact.js
@@ -2,23 +2,29 @@ import { Schema, model } from "mongoose";
 import { handleSaveError, addUpdateSettings } from "./hooks.js";
 import Joi from 'joi';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  }
-},
-    {versionKey: false, timestamps: true});
+  { versionKey: false, timestamps: true }
+);
   
 contactSchema.post("save", handleSaveError);
 contactSchema.pre("findOneAndUpdate", addUpdateSettings )
