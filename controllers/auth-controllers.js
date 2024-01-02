e441d7317037529f9b58bfe96200da80751dbfa1
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-02 19:11:38 +0200

    add

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,3 +1,13 @@
 import User from "../models/User.js";
 import { HttpError } from '../helpers/index.js';
 import { ctrlWrapper } from '../helpers/ctrlWrapper.js';
+
+const signup = (req, res) => {
+    
+}
+
+
+export default {
+  signup: ctrlWrapper(signup),
+
+};
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,4 +1,7 @@
 import express from 'express';
+
+import authControllers from '../../controllers/auth-controllers.js';
+
 import { userSignupSchema, userSigninSchema } from '../../models/User.js';
 
 import isEmptyBody from '../../middlewares/isEmptyBody.js';
@@ -7,7 +10,7 @@ import  validateBody  from '../../helpers/validateBody.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', isEmptyBody, validateBody(userSignupSchema))
+authRouter.post('/signup', isEmptyBody, validateBody(userSignupSchema),authControllers.signup)
 
 
 export default authRouter;
