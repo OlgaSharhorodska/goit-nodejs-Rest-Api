511417b3586d7245f7064779b284cce0331ffdba
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-02 18:22:24 +0200

    add auth to App

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
-import router from './routes/api/contacts-routes.js';
 import dotenv from 'dotenv';
+
+import router from './routes/api/contacts-routes.js';
+import authRouter from './routes/api/auth-router.js';
 
 dotenv.config();
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
+app.use('/api/auth', authRouter);
 app.use('/api/contacts', router);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: 'Not found' });
+});
 
 app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err;
- res.status(status).json({ message })
-})
+  res.status(status).json({ message });
+});
 
 export default app;
-
-
