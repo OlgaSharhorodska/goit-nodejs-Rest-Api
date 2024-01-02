17dc37f8011e6b37bb7e88c59e46175aa228db31
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-03 01:51:47 +0200

    add token

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,7 +1,10 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from "../models/User.js";
 import { HttpError } from '../helpers/index.js';
 import ctrlWrapper from '../helpers/ctrlWrapper.js';
+
+const { JWT_SECRET } = process.env;
 
 const signup = async (req, res) => {
     const { email,password } = req.body;
@@ -21,18 +24,26 @@ const signup = async (req, res) => {
 }
 
 const signin = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email })
-    if (!user) {
-        throw HttpError(401, 'Email or password is wrong');
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if (!passwordCompare) {
-        throw HttpError(401, "Email or password is wrong");
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, 'Email or password is wrong');
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, 'Email or password is wrong');
+  }
 
-    const token = "124.5333.555"
-    res.json({ token });
+  const { _id: id } = user;
+  const payload = {
+    id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '23h' });
+
+  res.json({
+    token,
+  });
 }
 
 export default {
