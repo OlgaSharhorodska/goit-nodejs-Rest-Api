d4a1e1e937bd3e5ebfb706678b1c92602d2774c8
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-02 18:42:27 +0200

    add model User

--- models/User.js
+++ models/User.js
@@ -2,14 +2,18 @@ import { Schema, model } from 'mongoose';
 import { handleSaveError, addUpdateSettings } from './hooks.js';
 import Joi from 'joi';
 
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const userSchema = new Schema(
   {
     password: {
       type: String,
       required: [true, 'Password is required'],
+      minlength: 6,
     },
     email: {
       type: String,
+      match: emailRegexp,
       required: [true, 'Email is required'],
       unique: true,
     },
@@ -25,3 +29,22 @@ const userSchema = new Schema(
   },
   { versionKey: false, timestamps: true }
 );
+
+userSchema.post('save', handleSaveError);
+userSchema.pre('findOneAndUpdate', addUpdateSettings);
+userSchema.post('findOneAndUpdate', handleSaveError);
+
+export const userSignupSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const userSigninSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model('user', userSchema);
+
+export default User;
