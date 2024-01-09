8a0ae82eda6a1250e4413875c78189bd1e6c7d26
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-09 20:27:02 +0200

    add upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -14,3 +14,22 @@ const storage = multer.diskStorage({
         callback(null, filename);
     }    
 })
+
+const limits = {
+    fileSize: 1024 * 1024 * 5,
+}
+
+const fileFilter = (req, file, callback) => {
+    const extention = req.originalname.split('.').pop();
+    if (extention==='exe') {
+        callback(HttpError(400,".exe isn't valid extention"))
+    }
+}
+
+const upload = multer({
+    storage,
+    limits,
+    fileFilter
+})
+
+export default upload;
