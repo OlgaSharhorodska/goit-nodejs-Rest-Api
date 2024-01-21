95596584358b6b6286d059ea1dd14f4ae5578709
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-01-21 21:51:02 +0200

    add User schema

--- helpers/index.js
+++ helpers/index.js
@@ -2,3 +2,4 @@ export { default as HttpError } from './httperror.js';
 export { default as isValidId } from '../middlewares/isValidId.js';
 export { default as validateBody } from './validateBody.js';
 export { default as ctrlWrapper } from './ctrlWrapper.js';
+export {default as sendEmail} from './sendEmail.js'
